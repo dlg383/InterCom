6bfd3459d430919ccadcde675ef4031f8dba18ca
Juli <jjulian1407@gmail.com>
2023-10-16 22:55:37 +0200

    NUevo

--- src/NAT_minimal.py
+++ src/NAT_minimal.py
@@ -7,19 +7,34 @@ class Main:
         'stun.cheapvoip.com',
         'stun.gmx.de',
         'stun.gmx.net',
-        ]
+    ]
 
-    nat_type = [] * 5
-    external_ip = [] * 5
-    external_port = [] * 5
+    def __init__(self):
+        first_port = None  # Variable para almacenar el primer puerto
+        hay_puertos_diferentes = False  # Variable para indicar si hay puertos diferentes
 
-    for stun_servers in stun_servers:
-        nat_type, external_ip, external_port = stun.get_ip_info(stun_host= stun_servers)
+        for stun_server in self.stun_servers:
+            nat_type, external_ip, external_port = stun.get_ip_info(stun_host=stun_server)
 
-        print("Server:" + stun_servers)
-        print("NAT Type:" + str(nat_type))
-        print("External IP:" + str(external_ip))
-        print("External Port:" + str(external_port) + "\n")
-            
+            print("Server:" + stun_server)
+            print("NAT Type:" + str(nat_type))
+            print("External IP:" + str(external_ip))
+            print("External Port:" + str(external_port))
+
+            # Si es la primera iteración, asigna el puerto a la variable first_port
+            if first_port is None:
+                first_port = external_port
+            else:
+                # Comparar el puerto actual con el primer puerto
+                if external_port == first_port:
+                    print(f"El puerto es igual a {first_port}!\n")
+                else:
+                    print(f"El puerto NO es igual a {first_port}.\n")
+                    hay_puertos_diferentes = True
+
+        if hay_puertos_diferentes:
+            print("La conexión es simétrica.")
+        else:
+            print("La conexión no es simétrica.")
 
 a = Main()
---
+++ src/NAT_transversal.py
@@ -0,0 +1,14 @@
+from NAT_minimal import Main
+
+class ClaseHereda(Main):
+    def __init__(self):
+        super().__init__()
+        # Puedes añadir código adicional si es necesario
+
+
+
+
+
+if __name__ == "__main__":
+    mi_instancia = ClaseHereda()
+    mi_instancia.run()
