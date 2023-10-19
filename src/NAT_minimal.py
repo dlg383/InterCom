ca411be14c7142495c780d7aab3ea2eafc08ca0d
Juli <jjulian1407@gmail.com>
2023-10-19 12:22:44 +0200

    Codigo NAT_minimal.py arreglado

--- src/NAT_minimal.py
+++ src/NAT_minimal.py
@@ -6,35 +6,35 @@ class Main:
         'stun.ekiga.net',
         'stun.cheapvoip.com',
         'stun.gmx.de',
-        'stun.gmx.net',
     ]
 
     def __init__(self):
         first_port = None  # Variable para almacenar el primer puerto
-        hay_puertos_diferentes = False  # Variable para indicar si hay puertos diferentes
+        puertos = set()  # Conjunto para almacenar los puertos
+        no_simetrica = True
+        ip = None
+        puerto = None
 
         for stun_server in self.stun_servers:
             nat_type, external_ip, external_port = stun.get_ip_info(stun_host=stun_server)
 
-            print("Server:" + stun_server)
-            print("NAT Type:" + str(nat_type))
-            print("External IP:" + str(external_ip))
-            print("External Port:" + str(external_port))
+            puertos.add(external_port)  # Agregar el puerto al conjunto
 
-            # Si es la primera iteración, asigna el puerto a la variable first_port
             if first_port is None:
                 first_port = external_port
-            else:
-                # Comparar el puerto actual con el primer puerto
-                if external_port == first_port:
-                    print(f"El puerto es igual a {first_port}!\n")
-                else:
-                    print(f"El puerto NO es igual a {first_port}.\n")
-                    hay_puertos_diferentes = True
+            elif external_port != first_port:
+                no_simetrica = True
+                ip = external_ip
+                puerto = external_port
 
-        if hay_puertos_diferentes:
-            print("La conexión es simétrica.")
+           # print("Server:" + stun_server)
+            #print("NAT Type:" + str(nat_type))
+            #print("External IP:" + str(external_ip))
+            #print("External Port:" + str(external_port))
+
+        if no_simetrica:
+            print(f"La conexión no es simétrica. IP: {external_ip}, Puerto: {external_port}")
         else:
-            print("La conexión no es simétrica.")
+            print("La conexión es simétrica.")
 
 a = Main()
--- src/NAT_transversal.py
+++ src/NAT_transversal.py
@@ -1,14 +1,15 @@
 from NAT_minimal import Main
+#from minimal import Minimal
 
 class ClaseHereda(Main):
     def __init__(self):
         super().__init__()
         # Puedes añadir código adicional si es necesario
 
-
+#class Min(Minimal):
 
 
 
 if __name__ == "__main__":
-    mi_instancia = ClaseHereda()
-    mi_instancia.run()
+   mi_instancia = ClaseHereda()
+   mi_instancia.run()
