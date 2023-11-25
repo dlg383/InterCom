8d3b3a55fe431579f79475fae61579bd10eeee9d
Juli <jjulian1407@gmail.com>
2023-11-25 13:12:24 +0100

    HITO 1: cambios seguramente finales

--- src/NAT_transversal.py
+++ src/NAT_transversal.py
@@ -1,11 +1,18 @@
 import sounddevice as sd
 import minimal
 import soundfile as sf
+from NAT_minimal import Main
 
-class Obtener_ip_puerto():
+class Obtener_ip_puerto(Main):
+
+
+    def __init__(self):
+        super().__init__()
+
     def obtener_ip_puerto(self):
         destination_address = input("Ingrese la dirección IP de destino: ")
-        
+        listening_port = input("Ingrese el puerto de escucha: ")
+
         while True:
             destination_port = input("Ingrese el puerto de destino: ")
             
@@ -15,16 +22,16 @@ class Obtener_ip_puerto():
             except ValueError:
                     print("\033[91mEl puerto debe ser un número entero.\033[0m")
             
-        return destination_address, destination_port
+        return destination_address, listening_port, destination_port
 
 # Obtén la dirección IP y el puerto del usuario
 obtener_puerto = Obtener_ip_puerto()
-destination_address, destination_port = obtener_puerto.obtener_ip_puerto()
+destination_address, listening_port, destination_port = obtener_puerto.obtener_ip_puerto()
 
 # Modifica directamente las opciones en minimal.parser
-minimal.parser.set_defaults(destination_address=destination_address, destination_port=int(destination_port))
+minimal.parser.set_defaults(destination_address=destination_address, listening_port=int(listening_port), destination_port=int(destination_port))
 
 # Ejecuta el script minimal.py
 minimal.args = minimal.parser.parse_args()
-minimal.intercom = minimal.Minimal()  # Puedes cambiar a Minimal() si no necesitas la versión verbose
+minimal.intercom = minimal.Minimal()
 minimal.intercom.run()
